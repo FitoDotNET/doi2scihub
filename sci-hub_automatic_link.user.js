239b3e6b77b1c23c06814bf1dd6602caf606d90b
FitoDotNET <37235643+FitoDotNET@users.noreply.github.com>
2019-07-11 21:53:39 -0400

    Update sci-hub_automatic_link.user.js

--- sci-hub_automatic_link.user.js
+++ sci-hub_automatic_link.user.js
@@ -1,25 +1,19 @@
 // ==UserScript==
 // @name        sci-hub automatic link
-// @website     http://blog.dorian-depriester.fr
-// @author      Dorian Depriester
-// @description Automatically turns each DOI link into a new link to the corresponding document on sci-hub.cc
-// @downloadURL https://github.com/DorianDepriester/doi2scihub/raw/master/sci-hub_automatic_link.user.js
-// @supportURL  https://github.com/DorianDepriester/doi2scihub/issues
-// @version     5.4
+// @website     http://fito.md
+// @author      Rodolfo Marin
+// @description Automatically turns each DOI link in pubmed into a new link to the corresponding document on sci-hub.cc. Original creation from Dorian Depriester http://blog.dorian-depriester.fr
+// @downloadURL https://github.com/FitoDotNET/doi2scihub/raw/master/sci-hub_automatic_link.user.js
+// @supportURL  https://github.com/FitoDotNET/doi2scihub/issues
+// @version     1.0
 // @grant       none
-// @match       *://*/*
-// @exclude     http*://*.google.*
-// @exclude     http*://*.stackexchange.com/*
-// @exclude     http*://*.stackoverflow.com/*
-// @exclude     http*://*.superuser.com/*
-// @exclude     http*://*.askubuntu.com/*
-// @exclude     http*://*.reddit.com/*
+// @match        *://*.ncbi.nlm.nih.gov/*
 // @run-at      document-idle
 // ==/UserScript==
 window.addEventListener('load', function() {
    var els = document.getElementsByTagName("A");
    for(var i = 0, l = els.length; i < l; i++) {
       var el = els[i];
-      el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.tw/');
+      el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://whereisscihub.now.sh/go/');
    }
 }, false);
