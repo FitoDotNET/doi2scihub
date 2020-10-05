3a35913214576c297bf01e63968b6652acc4749b
Rodolfo Marín <37235643+FitoDotNET@users.noreply.github.com>
2020-10-05 12:00:40 -0300

    Update sci-hub_automatic_link.user.js
    Edited sci-hub link, and add some journals

--- sci-hub_automatic_link.user.js
+++ sci-hub_automatic_link.user.js
@@ -5,15 +5,19 @@
 // @description Automatically turns each DOI link in pubmed into a new link to the corresponding document on sci-hub.cc. Original creation from Dorian Depriester http://blog.dorian-depriester.fr
 // @downloadURL https://github.com/FitoDotNET/doi2scihub/raw/master/sci-hub_automatic_link.user.js
 // @supportURL  https://github.com/FitoDotNET/doi2scihub/issues
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @match        *://*.ncbi.nlm.nih.gov/*
+// @match        *://*.nejm.org/*
+// @match        *://*.jamanetwork.com/*
+// @match        *://*.pubs.asahq.org/*
+// @match        *://*.journals.lww.com/*
 // @run-at      document-idle
 // ==/UserScript==
 window.addEventListener('load', function() {
    var els = document.getElementsByTagName("A");
    for(var i = 0, l = els.length; i < l; i++) {
       var el = els[i];
-      el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://whereisscihub.now.sh/go/');
+      el.href = el.href.replace(/^((http(s|):\/\/|www\.)|)(dx.|)doi\.org\//gim, 'http://sci-hub.se/');
    }
 }, false);
